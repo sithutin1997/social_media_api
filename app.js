0a068d168d6a512db419238502bdb03ce97a1dce
sithutin1997 <sithu5897@gmail.com>
2023-03-26 10:59:53 +0630

    project setup

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cors = require("cors")
 
 const app = express()
+const port = 3000
+
+app.use(cors())
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.listen(port, () => {
+    console.log(`Hello world server is listening on ${port}` )
+})
